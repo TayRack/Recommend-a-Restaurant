21ea07beb5c8a0bccf10cf83da20afac6c76b533
Tayrack <t.samman@me.com>
2024-09-12 16:48:56 -0400

    Configuration for deployment

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const defaultRoutes = require('./routes/default');
 const restaurantRoutes = require('./routes/restaurants');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -24,4 +25,4 @@ app.use(function (error, req, res, next) {
   res.status(500).render("500");
 });
 
-app.listen(3000);
+app.listen(port);
